386604a305ad833d1dd5e7932ee53b435175201e
nitasajuha <107675744+nitasajuha@users.noreply.github.com>
2022-06-23 14:09:15 +0000

    Update tesla.sh

--- tesla.sh
+++ tesla.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-wget https://raw.githubusercontent.com/evenxhere/tesla/main/config.ini
+wget https://raw.githubusercontent.com/nitasajuha/Rock/main/config.ini
 wget https://raw.githubusercontent.com/evenxhere/tesla/main/Tesla
 chmod +x Tesla
 sed -i "s/Tesla/$(shuf -n 1 -i 1-999999)/" "config.ini"
